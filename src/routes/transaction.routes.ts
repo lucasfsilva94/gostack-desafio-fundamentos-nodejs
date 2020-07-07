8997c04ebd4675ec5b9722278d764fd4a73ad606
Lucas Faria <lucasfsilva94@hotmail.com>
2020-07-06 22:35:46 -0300

    fix: add return to function

--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -28,7 +28,7 @@ transactionRouter.post('/', (request, response) => {
 
     const transaction = createTransaction.execute({ title, value, type });
 
-
+    return response.json(transaction);
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
